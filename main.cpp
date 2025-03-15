46ec3dd353e3bd2bfcf09902200dc328eb110d38
Nennneko5787 <nennneko5787@gmail.com>
2025-03-15 14:54:54 +0900

    oke

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "hsp3r.h"
 
 #define _HSP3CNV_DATE "2025/03/15"
-#define _HSP3CNV_TIME "14:34:19"
+#define _HSP3CNV_TIME "14:54:45"
 #define _HSP3CNV_MAXVAR 0
 #define _HSP3CNV_MAXHPI 32
 #define _HSP3CNV_VERSION 0x360
@@ -38,6 +38,9 @@ void __HspEntry( void ) {
 }
 
 static void L0003( void ) {
+	// redraw 0
+	PushInt(0); 
+	Extcmd(27,1);
 	// redraw 1
 	PushInt(1); 
 	Extcmd(27,1);
@@ -59,6 +62,13 @@ static void L0000( void ) {
 }
 
 static void L0001( void ) {
+	// dialog "キーボードオープン！"
+	PushStr("キーボードオープン！"); 
+	Extcmd(3,1);
+	TaskSwitch(5);
+}
+
+static void L0005( void ) {
 	//  "open_keyboard"
 	PushStr("open_keyboard"); 
 	Extcmd(74,1);
@@ -72,8 +82,8 @@ static void L0002( void ) {
 	// stop 
 	Prgcmd(17,0);
 	return;
-	// goto 
-	Prgcmd(0,0);
+	// goto *L0002
+	TaskSwitch(2);
 	return;
 }
 
@@ -85,6 +95,7 @@ CHSP3_TASK __HspTaskFunc[]={
 (CHSP3_TASK) L0002,
 (CHSP3_TASK) L0003,
 (CHSP3_TASK) L0004,
+(CHSP3_TASK) L0005,
 (CHSP3_TASK) 0,
 
 };
